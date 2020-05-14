fa7b575f67e7fc1e11ae20a700f390f8585c92e9
Sunghyuk Choi <likeaglow@gmail.com>
2020-05-15 02:08:24 +0900

    검색 구현1

--- src/main/java/com/board/service/BoardServiceImpl.java
+++ src/main/java/com/board/service/BoardServiceImpl.java
@@ -69,4 +69,11 @@ public class BoardServiceImpl implements BoardService {
 
         return dao.listPage(displayPost, postNum);
     }
+
+    // 게시물 목록 + 페이징 + 검색
+    @Override
+    public List<BoardVO> listPageSearch(int displayPost, int postNum, String searchType, String keyword)
+            throws Exception {
+        return dao.listPageSearch(displayPost, postNum, searchType, keyword);
+    }
 }
